e85053777e67c17b8b10d07ffffd66294edd4f8c
Murat Denizli <extmuratde@d-teknoloji.com.tr>
2021-10-07 01:51:04 +0300

    expense tracking list added

---
+++ src/components/Login.tsx
@@ -0,0 +1,74 @@
+import { Button, Form, Input, Result } from "antd";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useLocation } from "react-router";
+import { AppState } from "../store";
+import { login } from "../store/actions/userActions";
+import { LoginForm } from "../types/user";
+import showError from "../utils/showError";
+import showSuccess from "../utils/showSuccess";
+
+export default function Login() {
+  const history = useHistory();
+  const location = useLocation<{ newSignUp?: boolean }>();
+  const dispatch = useDispatch();
+
+  const { data, loading, error } = useSelector((state: AppState) => state.user);
+
+  const onFinish = (values: LoginForm) => {
+    dispatch(login(values));
+  };
+
+  useEffect(() => {
+    error && showError(error);
+  }, [error]);
+
+  useEffect(() => {
+    data.username && showSuccess("You have successfully logged in!");
+  }, [data.username]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) history.push("/");
+  }, [data]);
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      //   onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <h2 style={{ textAlign: "center", marginBottom: 10 }}>Please Login</h2>
+      {location.state?.newSignUp && (
+        <Result
+          status="success"
+          title="You successfully signed up!"
+          subTitle="Please login using your credentials."
+        />
+      )}
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
